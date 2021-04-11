92b4a54c52b552b6099535fb64a14a161e7b7751
Ana Paula <almeidaribeiroanapaula@gmail.com>
2021-04-11 13:24:42 -0300

    acionando exemplo 2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const calcular = require('./calculadora');
 
+const exemp = require('./exemplos');
+
 console.log(calcular().soma(10, 20));
 
 console.log(calcular().subtrair(50, 20));
 
 console.log("ola minha casa");
+
